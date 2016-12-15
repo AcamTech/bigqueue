dc40de86fe98504aeae0d6fd76c0ea48b1661ac1
Gabriel Andrés Eisbruch <geisbruch@arar017416.ml.com>
2016-12-15 18:48:31 -0300

    Retro compatible with old node versions

--- bin/os_http_admin_launcher.js
+++ bin/os_http_admin_launcher.js
@@ -55,7 +55,7 @@ if (cluster.isMaster) {
     cluster.fork();
   }
   cluster.on('exit',function(worker, code, signal) {
-    console.log(`worker ${worker.process.pid} died --> Creating new fork`);
+    console.log("worker "+worker.process.pid+" died --> Creating new fork");
     cluster.fork();
   }
 } else {
